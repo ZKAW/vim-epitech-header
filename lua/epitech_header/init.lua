f9cd29058fdb832e002a5def1c96add79d02d1d7
ZKAW <zkaw.exod@gmail.com>
2024-05-20 00:03:15 +0200

    style: make templating more generic

--- lua/epitech_header/init.lua
+++ lua/epitech_header/init.lua
@@ -1,60 +1,60 @@
-local function insert_epitech_header()
-    -- Extract project name
-    local buf_name = vim.api.nvim_buf_get_name(0)
-    local file_name = vim.fn.fnamemodify(buf_name, ':t')
-    local file_description = vim.fn.fnamemodify(file_name, ':r')
-    local project_name = vim.fn.fnamemodify(vim.loop.cwd(), ':t')
-    -- Extract the current file type
-    local file_type = vim.bo.filetype
-
-    -- Define the header content based on the file type
-    local header
-    if file_type == 'c' or file_type == 'cpp' then
-        header = string.format([[
+local header_templates = {
+    c = [[
 /*
 ** EPITECH PROJECT, %s
 ** %s
 ** File description:
 ** %s
 */
-]], os.date("%Y"), project_name, file_description)
-    elseif file_type == 'python' then
-        header = string.format([[
+]],
+    cpp = [[
+/*
+** EPITECH PROJECT, %s
+** %s
+** File description:
+** %s
+*/
+]],
+    python = [[
 """
 EPITECH PROJECT, %s
 %s
 File description:
 %s
 """
-]], os.date("%Y"), project_name, file_description)
-    elseif file_type == 'sh' then
-        header = string.format([[
+]],
+    sh = [[
 #!/bin/bash
 # EPITECH PROJECT, %s
 # %s
 # File description:
 # %s
-]], os.date("%Y"), project_name, file_description)
-elseif file_type == 'haskell' then
-    header = string.format([[
+]],
+    haskell = [[
 {-
 -- EPITECH PROJECT, %s
 -- %s
 -- File description:
 -- %s
 -}
-]], os.date("%Y"), project_name, file_description)
-    else
-        -- Default header (similar to C/C++ style)
-        header = string.format([[
-/*
-** EPITECH PROJECT, %s
-** %s
-** File description:
-** %s
-*/
-]], os.date("%Y"), project_name, file_description)
-    end
+]],
+}
+
+local function insert_epitech_header()
+    -- Extract project name
+    local buf_name = vim.api.nvim_buf_get_name(0)
+    local file_name = vim.fn.fnamemodify(buf_name, ':t')
+    local file_description = vim.fn.fnamemodify(file_name, ':r')
+    local project_name = vim.fn.fnamemodify(vim.loop.cwd(), ':t')
+
+    -- Extract the current file type
+    local file_type = vim.bo.filetype
+
+    -- Look up the template for the current file type
+    local template = header_templates[file_type] or header_templates['c']
+
+    -- Generate the header using the template
+    local header = string.format(template, os.date("%Y"), project_name, file_description)
 
     -- Insert the header at the top of the current buffer
     vim.api.nvim_buf_set_lines(0, 0, 0, false, vim.split(header, '\n'))
@@ -79,4 +79,3 @@ return {
     insert_epitech_header = insert_epitech_header,
     setup = setup
 }
-
